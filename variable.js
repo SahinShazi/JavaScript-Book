fb3b5ef1042cf7c2b796addf716fa0a9a667c3a6
SahinShazi <rksahinrone@gmail.com>
2025-08-13 18:08:26 +0600

    update variable.js

--- variable.js
+++ variable.js
@@ -1,4 +1,5 @@
 //Homework
+//Variable Declaration
 const birthday = 2000;
 const brother = 1;
 const students = 450;
@@ -6,3 +7,25 @@ const marks = 70;
 const rooms = 2;
 const hours = 12;
 const total = 20383973798373768373;
+
+//How to use console.log():
+const age = 17;
+console.log(age);
+
+const bookToRead = 20;
+console.log(bookToRead);
+
+const wakeUpTime = 5;
+console.log(wakeUpTime);
+
+const wastedTime = 2;
+console.log(wastedTime);
+
+const borrow = 1000;
+console.log(borrow);
+
+const goals = 20;
+console.log(goals);
+
+const pillow = 2;
+console.log(pillow);
