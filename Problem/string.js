623f758ad1c4cf0984f1d75d1d4ed8d8cb815b0b
SahinShazi <rksahinrone@gmail.com>
2025-08-14 07:49:11 +0600

    update string.js

---
+++ Problem/string.js
@@ -0,0 +1,20 @@
+const name = "Sahin Enam";
+console.log(name);
+
+const address = "Barisal Bhola";
+console.log(address);
+
+const school = "চেয়ারম্যান বাজার হাই স্কুল";
+console.log(school)
+
+const fan = "ঝংকার মাহবুব";
+console.log(fan);
+
+const movieOfTheYear = "Tofan";
+console.log(movieOfTheYear);
+
+const hobby = "I like learning.";
+console.log(hobby);
+
+const color = 'Read';
+console.log(color);
