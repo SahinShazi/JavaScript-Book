471ecf6210b8a0afaa0e286da40d4adc4529b92b
SahinShazi <rksahinrone@gmail.com>
2025-08-14 08:05:30 +0600

    update trueFalse.js

---
+++ Problem/trueFalse.js
@@ -0,0 +1,26 @@
+const iHappy = true;
+console.log(iHappy);
+
+const crush = false;
+console.log(crush);
+
+const celebrating = true;
+console.log(celebrating);
+
+const isHappy = true;
+console.log(isHappy);
+
+const hasMoney = false;
+console.log(hasMoney);
+
+const isHungry = false;
+console.log(isHungry);
+
+const isStudyDone = true;
+console.log(isStudyDone);
+
+const isPhoneCharged = false;
+console.log(isPhoneCharged);
+
+const isDinnerDone = false;
+console.log(isDinnerDone);
