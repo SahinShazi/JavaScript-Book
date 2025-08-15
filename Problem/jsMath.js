46f40d9877d8ae3e8423a7e75ae1e9a49bf7d18b
SahinShazi <rksahinrone@gmail.com>
2025-08-15 10:51:37 +0600

    update jsMath.js

---
+++ Problem/jsMath.js
@@ -0,0 +1,49 @@
+//Practice -1
+const myIncome = 100;
+const expenses = 40;
+const total = myIncome - expenses;
+console.log(total); //The ans: 60
+
+//Practice -2
+const pencil = 10;
+console.log(pencil / 2); //The ans: 5
+
+//Practice -3
+const money = 60;
+console.log(money / 15); //The ans: 4
+console.log(money % 15); //The ans: 0
+
+//Practice -4
+const text1 = "Hello ";
+const text2 = "World";
+const addition = text1 + text2;
+console.log(addition); //The ans is "Hello World"
+
+//Practice -5
+const rice = 153;
+const pulses = 261;
+console.log(rice + pulses); //Total 414kg
+
+//Practice -6
+const haveMoney = 500;
+console.log(haveMoney / 75); //The ans is: 6tk
+console.log(haveMoney % 75); //The ans is: 50tk
+
+//Practice -7
+console.log(8 / 3); //The ans is: 2
+console.log(8 % 3); //The ans is: 2
+
+//Practice -8
+const money1 = 50;
+console.log(50 % 9); //The ans is: 5
+
+//Practice -9
+const bangla = 'Bangla';
+const desh = 'desh';
+console.log(bangla + desh); //The ans is: Bangladesh
+
+//Practice -10
+const rice1 = 480; //mon = 12
+const ans = rice1 / 4;
+console.log(ans); //The ans: 120 days 
+console.log(ans / 30); //Total 4 months
