f491d0504bd7aeb6d014b1fd2daa1d9142232c5b
SahinShazi <rksahinrone@gmail.com>
2025-08-15 11:25:40 +0600

    update type.js

---
+++ Problem/type.js
@@ -0,0 +1,29 @@
+//Practice-1
+const price = 555;
+console.log(typeof price);
+//The answer is: number 
+
+// Practice-2
+const learning = true;
+console.log(typeof learning);
+//The answer is: boolean
+
+//Practice-3
+const megaCity = "Dhaka";
+console.log(typeof megaCity);
+//The answer is: string 
+
+//Practice-4
+const temperature = 102.5;
+console.log(typeof temperature);
+//The answer is number 
+
+//Practice-5
+const isHungry = true;
+console.log(typeof isHungry);
+//The answer is boolean 
+
+//Practice-6
+const isRice = true;
+console.log(typeof isRice);
+//The answer is boolean 
