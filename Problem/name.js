a90a085e0c1bd263b55b155983b838fb585a3390
SahinShazi <rksahinrone@gmail.com>
2025-08-15 20:10:53 +0600

    update name.js

---
+++ Problem/name.js
@@ -0,0 +1,35 @@
+//Practice -1
+let myFavoriteFood = 'Apple';
+
+//Practice-2
+let city = "Chitterling";
+let City = "Dhaka";
+console.log(city);
+console.log(City);
+//There is case sensitive 
+
+//Practice-3
+let userName = "Jovan";
+let username = "Johan";
+console.log(userName);
+console.log(username);
+//There is case sensitive 
+
+//Practice-4
+let myAddress = "I don't know!";
+console.log(myAddress);
+
+//Practice-5
+//let super = 30;
+//console.log(super);
+//This is violet JavaScript rules 
+
+//Practic-6
+/*let class = 20;
+console.log(class);
+This is violet JavaScript rules */
+
+//Practice-7
+/*let 2ndPosition = "Sahin Enam';
+console.log(2ndPosition);
+This is violet JavaScript rules */
