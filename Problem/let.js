c1ae64481395bd5184719322fd9451d317a13adb
SahinShazi <rksahinrone@gmail.com>
2025-08-15 11:51:48 +0600

    update let.js

---
+++ Problem/let.js
@@ -0,0 +1,36 @@
+//Practice-1
+let age = 17;
+console.log(age); //Ans: 17
+age = 20;
+console.log(age); //Ans: 20
+
+//Practice-2
+let pocketMoney = 500;
+pocketMoney = 150;
+console.log(pocketMoney); //Ans: 150
+
+//Practice-3
+let studyTime = 4;
+studyTime = 2;
+console.log(studyTime); //Ans: 2
+
+//Practice-4
+let friendsCount = 3;
+friendsCount = 1;
+console.log(friendsCount); //Ans: 1
+
+//Practice-5
+let phonePrice = 25000;
+phonePrice = 12000;
+console.log(phonePrice); //12000
+
+//Practice-6
+let chairsCount = 40;
+chairsCount = 38;
+chairsCount = 32;
+console.log(chairsCount); //32
+
+//Practice-7
+let playTime = 3;
+playTime = 0;
+console.log(playTime); //0
