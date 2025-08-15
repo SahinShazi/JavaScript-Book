952ed2e4ada4dbd4236a811180a990235797ad88
SahinShazi <rksahinrone@gmail.com>
2025-08-15 22:41:17 +0600

    update parse.js

---
+++ Problem/parse.js
@@ -0,0 +1,39 @@
+//Practice-1
+let number = "20";
+number = parseInt(number);
+console.log(number + 10); //Ans: 30
+
+//Practice-2
+let number1 = '3.14159';
+number1 = parseInt(number1);
+console.log(number1);
+
+//Practice-3
+let text = 'premikBoy';
+text = parseInt(text);
+console.log(text); //Ans: Not a Number(NaN)
+
+//Practice-4
+let number2 = 3.456;
+let number3 = 2.789;
+let total = number2 + number3;
+let totalNumber = (total.toFixed(2));
+console.log(totalNumber); //Ans: 6.25
+
+//Practice-5
+let num1 = '56.78';
+let num2 = '12.34';
+let sum = parseFloat(num1 + num2);
+console.log(sum); //Ans: 56.7812
+
+//Practice-6
+let num3 = 10.5678;
+let num4 = 5.4321;
+console.log(num3.toFixed(1));
+console.log(num4.toFixed(1));
+
+
+//Practice-7
+let num5 = '42.45689754';
+num5 = parseFloat(num5);
+console.log(num5.toFixed(3));
