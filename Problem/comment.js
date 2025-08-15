ee866b4b3f5402afc59231e5a0e900fb4bc29e67
SahinShazi <rksahinrone@gmail.com>
2025-08-15 18:12:29 +0600

    update comment.js

---
+++ Problem/comment.js
@@ -0,0 +1,36 @@
+//Practice-1
+//football score
+let score = 40;
+
+//Practice-2
+//This is the box radius of a button. 
+//This is used to score the four corners of the box.
+//This is done to secure the box. 
+let radius = 10//px;
+
+//Practice-3
+/*Multiline comments have been made here. 
+Multiline comments are made like this.*/
+let dress = "dress"
+
+//Practice-4
+//This was given to me for my birthday.
+let giftMoney = 100;
+
+//Practice-5
+/*এই সাত ঘন্টার ভিতরে আমি আমার 
+পূর্ব প্রোগ্রামিং এর যত কি যা শিখেছি 
+সব কিছু ডিভিশন দিব। এবং পাশাপাশি 
+আরো নতুন কিছু শিখার চেষ্টা করব যেটা 
+আমার ওয়েব ডেভলপার হওয়ার পেছনে সাহায্য করবে*/
+let studyTime = 7;
+
+//Practice-6
+/*He used to get some money from me and 
+I haven't given it to him for a long time, 
+but I'm giving it to him now.*/
+let sendMoney = 300;
+
+//Practice-7
+//delivery cost and product cost = 2000tk
+let pay = 2000;
