2e79f22b60418cf48179455e3a6277e2d9ee9f5a
SahinShazi <rksahinrone@gmail.com>
2025-08-16 22:37:28 +0600

    update

--- Problem/variableComparison.js
+++ Problem/variableComparison.js
@@ -1,4 +1,27 @@
 //Practice 1
+console.log(7 > 5); //true
+
+//Practice 2
+console.log(45 < 50); //true
+
+//Practice 3
+console.log(75 == 75); //true 
+
+//Practice 4
+console.log(100 == 95); //false 
+
+//Practice 5
+console.log(10 == 7); //false
+
+//Practice 6
+console.log(20 <= 15); //false 
+
+//Practice 7
+console.log(45 >= 97); //false 
+
+//----------------------------------------------------------------------
+
+//Practice 1
 let myBrotherAge = 10;
 let myAge = 15;
 console.log(myBrotherAge > myAge); //false 
