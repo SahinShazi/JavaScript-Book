1b03bebaaa3e1a7b8c504331cef9789c7d75dcda
SahinShazi <rksahinrone@gmail.com>
2025-08-16 22:34:43 +0600

    update

---
+++ Problem/comparison.js
@@ -0,0 +1,20 @@
+//Practice 1
+console.log(7 > 5); //true
+
+//Practice 2
+console.log(45 < 50); //true
+
+//Practice 3
+console.log(75 == 75); //true 
+
+//Practice 4
+console.log(100 == 95); //false 
+
+//Practice 5
+console.log(10 == 7); //false
+
+//Practice 6
+console.log(20 <= 15); //false 
+
+//Practice 7
+console.log(45 >= 97); //false 
--- Problem/variableComparison.js
+++ Problem/variableComparison.js
@@ -1,20 +1,13 @@
 //Practice 1
-console.log(7 > 5); //true
+let myBrotherAge = 10;
+let myAge = 15;
+console.log(myBrotherAge > myAge); //false 
 
 //Practice 2
-console.log(45 < 50); //true
+let myMarks = 45;
+console.log(myMarks > 33); //true 
 
 //Practice 3
-console.log(75 == 75); //true 
-
-//Practice 4
-console.log(100 == 95); //false 
-
-//Practice 5
-console.log(10 == 7); //false
-
-//Practice 6
-console.log(20 <= 15); //false 
-
-//Practice 7
-console.log(45 >= 97); //false 
+const jambur = "Jambur";
+const kadabel = 'Kadabel';
+console.log(jambur < kadabel); //true 
