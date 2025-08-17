36d417fa186cb46f0b62c57f3c01d604177803ec
SahinShazi <rksahinrone@gmail.com>
2025-08-17 15:26:58 +0600

    update condition.js

---
+++ Problem/condition.js
@@ -0,0 +1,30 @@
+//Practice 1
+const backToHome = 6;
+
+if (backToHome <= 6) {
+    console.log("Thank you for coming before six.");
+} else {
+    console.log("Come, I will punish you.");
+} //ans: Thank you for coming before six.
+
+
+
+//Practice 2
+const login = true;
+
+if (login == true) {
+    console.log("Welcome! The login is successful.");
+} else {
+    console.log("Sorry! Get lost!");
+} //ans: Welcome! The login is successful.
+
+
+
+//Practice-3
+const iRun = true;
+
+if (iRun == true) {
+    console.log("You survived, friend.");
+} else {
+    console.log("Abar ki hobe friend? aso!");
+} //ans: You survived, friend.
