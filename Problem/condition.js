84533832777e0bf8c957681976b5ad5483be0052
SahinShazi <rksahinrone@gmail.com>
2025-08-17 21:11:55 +0600

    update condition.js

--- Problem/condition.js
+++ Problem/condition.js
@@ -28,3 +28,33 @@ if (iRun == true) {
 } else {
     console.log("Abar ki hobe friend? aso!");
 } //ans: You survived, friend.
+
+
+//Practice 4
+const marks= 85;
+if(marks >= 80) {
+    console.log("Congratulations! You passed. You will get a bike.");
+} else{
+    console.log("You failed. You won't get a bike now.");
+} //Ans: Congratulations! You passed. You will get a bike.
+
+
+
+//Practice 5
+const startMovieBefore9 = false;
+
+if(startMovieBefore9 == true) {
+    console.log("I will watch a movie.");
+} else{
+    console.log("I don't watch a movie!");
+} //ans: I don't watch a movie!
+
+
+//Practice 6
+const temperature = 23;
+
+if(temperature >= 65) {
+    console.log("Hot temperature!");
+} else{
+    console.log('Very low temperature!');
+} //ans: Very low temperature!
