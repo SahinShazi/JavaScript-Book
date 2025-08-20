2ab545815b92067007ffcf737de4cfaf6fb7844f
SahinShazi <rksahinrone@gmail.com>
2025-08-20 16:52:33 +0600

    update Arrays.js

---
+++ Problem/Arrays.js
@@ -0,0 +1,33 @@
+//Arrays 
+//Practice 1
+const num2 = [71,72,73,74,75,76,77,78,79];
+
+//Practice 2
+const food = ["Apple", "Banana", "Orange", "Mango", "Guava"];
+console.log(food);
+console.log(food.length);
+
+//Practice 3
+const food1 = [
+  "আলু", "পেঁয়াজ", "বেগুন", "মুলা", "গাজর", 
+  "টমেটো", "ঢেঁড়স", "লাউ", "শসা", "পটল"
+];
+console.log(food1);
+console.log(food1.length); //Asns: 10
+
+//Practice 4
+const movie = ["তুফান", "প্রিয়তমা", "তান্ডব", "দাগি", "সর"];
+console.log(movie);
+
+//Practice 5
+const num1 = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19];
+console.log(num1);
+
+//Practice 6
+const color = ["Blue", "Black", "Red", "White", "Green", "Holud"];
+console.log(color.length); //Ans: 6
+
+//Practice 7
+const con = ["Bangladesh", "India", "Pakistan", "Turkey", "Saudi Arab", "America"];
+console.log(con);
+console.log(con.length) //6
