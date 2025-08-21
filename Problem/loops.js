23db4f1fed4618d693ceced9ea0af21b913c5968
SahinShazi <rksahinrone@gmail.com>
2025-08-21 19:23:11 +0600

    update loops.js

---
+++ Problem/loops.js
@@ -0,0 +1,30 @@
+//Problem 1
+let books = ["ICT", "Math", "Islam", "Bangla", "BGS", "Physics"];
+
+for (let book of books) {
+    console.log(book);
+}
+
+//Problem 2
+let foods = ["Mango", "Apple", "Orange", "Banana"];
+for (let food of foods) {
+    console.log(food);
+}
+
+//Problem 3
+let birth = [1999, 2000, 2017, 2010, 1989];
+for (let print of birth) {
+    console.log(print);
+}
+
+//Problem 4
+let player = ["Maruf", 'Jehad', 'Ismail', 'Rakib'];
+for (let players of player) {
+    console.log(players);
+}
+
+//Problem 5
+let date = ["2/7/2025", "7/5/2025", "3/10/2025", "5/7/2025"];
+for (let dates of date) {
+    console.log(dates);
+}
