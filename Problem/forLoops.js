055d8a4a5c9e16c1e607aaa8b473cbe99a67fad3
SahinShazi <rksahinrone@gmail.com>
2025-08-22 15:25:33 +0600

    update forLoops.js

---
+++ Problem/forLoops.js
@@ -0,0 +1,11 @@
+//Problem 1
+for (let num = 150; num <= 170; num++) {
+ console.log(num);
+}
+
+//Problem 2
+let sum5 = 0;
+for (let rolls = 31; rolls < 58; rolls++) {
+  sum5 = sum5 + rolls;
+}
+console.log(sum5);
