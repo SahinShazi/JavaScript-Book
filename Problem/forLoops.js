fa6097bfe65c81e86dd695348a04e9f4ac19bd98
SahinShazi <rksahinrone@gmail.com>
2025-08-22 22:17:06 +0600

    update forLoops.js

--- Problem/forLoops.js
+++ Problem/forLoops.js
@@ -58,3 +58,58 @@ for (let i = 1; i <= 50; i++) {
     console.log(i);
 } 
 } 
+
+
+//----------- Break and continue-----------
+//Problem 1
+for (let i = 1; i <= 30; i++) {
+  if (i > 15) {
+    break;
+  }
+  //console.log(i);
+}
+
+//Problem 2
+for (let i = 1; i <= 40; i++) {
+  if (i % 7 == 0) {
+    continue;
+  }
+ // console.log(i);
+}
+
+//Problem 3
+for (let i = 1; i <= 15; i++) {
+  if (i === 9) {
+    continue;
+  }
+  //console.log(i);
+}
+
+//Problem 4
+for (let i = 1; i <= 20; i++) {
+  if (i === 12) {
+    continue;
+  }
+  //console.log(i);
+}
+
+//Problem 5
+for (let i = 1; i <= 25; i++) {
+  if (i % 3 === 0) {
+    continue;
+  }
+ // console.log(i);
+} 
+
+
+//Problem 6
+for (let i = 91; i <= 120; i++) {
+  if (i % 10 === 0) {
+    break;
+  }
+//  console.log(i);
+}
+
+
+
+
