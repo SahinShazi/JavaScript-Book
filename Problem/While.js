31dbbbfbbc5da3db8df9d50df3e4dc339503182e
SahinShazi <rksahinrone@gmail.com>
2025-08-22 10:58:01 +0600

    update While.js

--- Problem/While.js
+++ Problem/While.js
@@ -32,3 +32,55 @@ while (num3 > 110) {
   console.log(num3 / 2);
   num3++
 }
+
+
+//------SUM--------
+//Problem 1
+let num4 = 50;
+let sum = 0;
+
+while (num4 <= 100) {
+    sum = sum + num4;
+    num4++
+}
+console.log('Total:', sum); //Total: 3825
+
+//Problem 2
+let num5 = 5
+let sum1 = 0;
+
+while (num5 <= 15) {
+  sum1 = sum1 + num5;
+  num5++
+}
+console.log('Total:', sum1); //Total: 110
+
+//Problem 3
+let num6 = 1;
+let sum2 = 0;
+
+while (num6 <= 50) {
+  sum2 = sum2 + num6;
+  num6++;
+}
+console.log('Total:', sum2) //Total: 1275
+
+
+//Problem 4
+let num7 = 21;
+let sum3 = 0;
+while (num7 <= 50) {
+  sum3 = sum3 + num7;
+  num7++
+}
+console.log('Total:', sum3);//Total: 1065
+
+//Problem 5
+let num8 = 20;
+let sum4 = 0;
+while (num8 <= 40) {
+  sum4 = sum4 + num8;
+  num8++
+}
+console.log('Total:', sum4); //Total: 630
+
