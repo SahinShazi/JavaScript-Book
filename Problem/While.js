aee0386d7b439cb91d04ac790688681b0ebbe946
SahinShazi <rksahinrone@gmail.com>
2025-08-22 10:10:00 +0600

    update While.js

---
+++ Problem/While.js
@@ -0,0 +1,34 @@
+//Problem 1
+let practice = 0;
+while (practice < 10) {
+  console.log("Ami every day minimum 3 hours practice korbo!");
+  practice++
+}
+
+//Problem 2
+let num = 0
+while (num <= 20) {
+  console.log(num);
+  num++
+}
+
+//Problem 3
+let num1 = 50;
+while (num1 <= 100) {
+  console.log(num1);
+  num1 ++;
+}
+
+//Problem 4
+let num2 = 1;
+while (num2 <= 10) {
+  console.log(num2 * 3);
+  num2 = num2 + 1;
+}
+
+//Problem 5
+let num3 = 111;
+while (num3 > 110) {
+  console.log(num3 / 2);
+  num3++
+}
