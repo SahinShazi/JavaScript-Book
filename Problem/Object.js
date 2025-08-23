2dae8f31a1b222ddd94c0b0b3dfc0d5a5146d637
SahinShazi <rksahinrone@gmail.com>
2025-08-23 11:03:37 +0600

    update Object.js

---
+++ Problem/Object.js
@@ -0,0 +1,56 @@
+//Problem 1
+const teacher = {
+  name: "Monir",
+  subject: ['BGS', 'English', 'Science'],
+  home: "Bhola",
+  school: "Hazariganj High School"
+}
+
+//Problem 2
+const tree = {
+  name: "Food",
+  age: 80,
+  height: 60,
+  food: "Apple"
+}
+
+//Problem 3
+const cat = {
+  name: "cat",
+  age: 7,
+  home: 'My home',
+  height: "13cm"
+}
+
+//Problem 4
+const father = {
+  name: "Jamal",
+  age: "60+",
+  passion: "Worker",
+  sonNum: 2
+}
+
+//Problem 5
+const motorcycle = {
+  brand: "Honda",
+  bane: "BMW",
+  age: 20,
+  color: "Green",
+  maxSpeed: "70km"
+}
+
+//Problem 6
+const bird = {
+  name: "PagasPaki",
+  food: "Rice",
+  color: "White"
+}
+
+//Problem 7
+const laptop = {
+  brand: "HP",
+  name: "28ch8",
+  type: "laptop",
+  display: "LCD",
+  processor: "Intel"
+}
