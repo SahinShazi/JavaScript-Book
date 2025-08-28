80915b0b0b3f7559d00282afb4caec6da5851ff8
SahinShazi <rksahinrone@gmail.com>
2025-08-28 08:57:36 +0600

    update funtion.js

---
+++ Problem/funtion.js
@@ -0,0 +1,61 @@
+//How to declare function 
+//Problem 1
+function juiceKao() {
+    console.log("Juice kayo");
+}
+
+juiceKao();
+
+//Problem 2
+function myRoutine() {
+    console.log("At first i wekup 4am and complete my all routine.");
+}
+
+myRoutine();
+
+
+//Problem 3
+function myFamily() {
+    console.log("My father name is Jamal");
+    console.log("My mother name is Minara");
+    console.log("My brother name is Shakil");
+    console.log("My name is Sahin Enam");
+}
+
+myFamily();
+
+
+//Problem 4
+function taskDone() {
+    console.log("I Complete my work, like read book, Practice programming, Homework.");
+}
+
+taskDone();
+
+//Problem 5
+function greetMe() {
+    console.log("Good Morning! Have a great day.");
+}
+
+greetMe();
+
+
+//Problem 6
+function getFavFriends() {
+    console.log(`Maruf, Nahim, Mohin and Emon.`);
+}
+
+getFavFriends();
+
+//Problem 7
+function introduceMyself() {
+    console.log("My name is Sahin.");
+    console.log("I am 16.");
+    console.log("Dhaka");
+    console.log("+8801732373069");
+    console.log("My height ...");
+    console.log("My favorite food is rice.");
+}
+//I can write one line 
+
+introduceMyself()
