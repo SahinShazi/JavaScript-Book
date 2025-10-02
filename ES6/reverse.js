720a513cec91fc28abd0064c6ecf11d391990beb
SahinShazi <rksahinrone@gmail.com>
2025-10-02 07:44:55 +0600

    update reverse.js

---
+++ ES6/reverse.js
@@ -0,0 +1,15 @@
+//JavaScript Reverse
+//Problem 1 
+const names = ["Sahin", "Maruf", "Nhahim", "Mohin", "Rakib"];
+names.reverse();
+console.log(names);
+
+//Problem 2 
+const someNumber = [1,2,3,4,5,6,7,8,9,10];
+someNumber.reverse();
+console.log(someNumber);
+
+//Problem3
+const users = [{name: "Ali", age: 30}, {name: 'Sahin', age: 19}, {name: 'Rakib', age: 26}];
+users.reverse();
+console.log(users);
