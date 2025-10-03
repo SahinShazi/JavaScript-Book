94f85af827b8c007e9b827929460f25d805d1503
SahinShazi <rksahinrone@gmail.com>
2025-10-03 08:05:40 +0600

    update immutable.js

---
+++ String Method/immutable.js
@@ -0,0 +1,24 @@
+//JavaScript String Method 
+//Immutable String
+//Problem 1 
+const city = 'Chattogram';
+console.log(city.indexOf('g'));
+
+//Problem 2 
+const division  = 'Sylhet';
+console.log(division.includes('y'));
+
+//Problem 3 
+const name = 'Rifat';
+console.log(name[4]);
+
+//Problem 4 
+const language = 'JavaScript';
+console.log(language.indexOf('rip'));
+
+//Problem 5
+const text = 'Immutable';
+console.log(text.length);
+
+text[0] = 'Y';
+console.log(text);
