78013549f8d4cea2d60df09b9c30d9be772a11c3
SahinShazi <rksahinrone@gmail.com>
2025-10-04 07:13:17 +0600

    update lowerUpper.js

---
+++ String Method/lowerUpper.js
@@ -0,0 +1,19 @@
+//JavaScript LowerUpper Case 
+//Problem 1 
+const email = 'RKSAHINRONE@GMAIL.COM';
+const toLowerCase = email.toLowerCase();
+console.log(toLowerCase);
+
+
+//Problem 2 
+const rendomeText = 'DHelOthGFjHGDfjwsfcGvdDG';
+const upperCase = rendomeText.toUpperCase();
+const lowerCase = rendomeText.toLowerCase();
+
+console.log(upperCase);
+console.log(lowerCase);
+
+//Problem 3 
+const language = 'JavaScript';
+const lower = language.toLowerCase();
+console.log(lower.includes('script'));
