56d76f3aa0697b599377e1fcec35f22b294aa919
SahinShazi <rksahinrone@gmail.com>
2025-10-05 08:51:37 +0600

    update split.js

---
+++ String Method/split.js
@@ -0,0 +1,43 @@
+//JavaScript Split 
+//Problem 1 
+const realFrinds = 'Raju,Rana,Hasan,Kabir,Mahi';
+console.log(realFrinds.split(','));
+
+//Problem 2 
+const frinds = ['Raju', 'Rana', 'Hasan', 'Kabir', 'Mahi'];
+console.log(frinds.join(','));
+
+//Problem 3 
+const functions = 'function if else while';
+console.log(functions.split(' '));
+
+//Problem 4 
+const languages = ["JavaScript", 'Python', 'Java'];
+console.log(languages.join(';'));
+
+
+//Problem 5 
+const text = 'for,while,for-in,for-of,do-while';
+console.log(text.split(','));
+
+
+//Problem 6 
+let sentance = ' console log debugger '
+console.log(sentance.trim());
+
+//Problem 7 
+let str1 = 'push'
+let str2 = 'pop'
+let str3 = 'shift'
+let str4 = 'unshift'
+
+console.log(str1.concat(' ', str2, ' ', str3, ' ', str4));
+
+
+//Problem 8 
+let sentance2 = "Hello JavaScript Developers";
+console.log(sentance2.substring(6, 16));
+
+//Problem 9 
+let sentance3 = "Code more learn more";
+console.log(sentance3.substring(10, 15));
