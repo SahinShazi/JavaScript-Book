a724d87fb9735b1c8c484748a03035c4daf3ef35
SahinShazi <rksahinrone@gmail.com>
2025-10-06 08:12:55 +0600

    update repeat.js

---
+++ String Method/repeat.js
@@ -0,0 +1,19 @@
+//Javascript Repeat
+//Problem 1 
+let text = "Hi";
+let strRepeat = text.repeat(10);
+console.log(strRepeat);
+
+
+//Problem 2 
+let text1 = "I am learing Javascript. Javascript is Amazing";
+let text1Repeat = text1.replace("Javascript", "JS");
+let text1Repeat1 = text1.replaceAll("Javascript", "JS");
+console.log(text1Repeat);
+console.log(text1Repeat1);
+
+
+//Problem 3 
+let text2 = "I love apples and apples are tasty";
+let text2RepleseAll = text2.replaceAll("apples", "mangies");
+console.log(text2RepleseAll);
