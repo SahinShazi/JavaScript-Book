14d6d222e9334647de83b1c64502f0094fb4d141
SahinShazi <rksahinrone@gmail.com>
2025-10-07 09:29:26 +0600

    update reverse.js

---
+++ String Method/reverse.js
@@ -0,0 +1,43 @@
+//JavaScript Reverse
+//Problem 1 
+let sentence = "Hello";
+let reverse = "";
+
+for (let letter of sentence) {
+  reverse = letter + reverse;
+}
+
+console.log(reverse);
+
+
+//Problem 2 
+let sentence1 = "I Love JavaScript";
+let reverse1 = "";
+
+for (let letter of sentence1) {
+  reverse1 = letter + reverse1;
+}
+
+console.log(reverse1);
+
+//Problem 3 
+let sentence2 = "Bangladesh";
+let reverse2 = "";
+
+for (let letter1 of sentence2) {
+  reverse2 = letter1 + reverse2;
+}
+
+console.log(reverse2);
+
+
+//Problem 4 
+let number = "1 2 3 4 5";
+let reverse3 = "";
+
+for (let letter of number) {
+  reverse3 = letter + reverse3;
+}
+
+console.log(reverse3);
+
