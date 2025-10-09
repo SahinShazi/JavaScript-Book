45264306b103ec8e46c65a9462a8d74035f28ba3
SahinShazi <rksahinrone@gmail.com>
2025-10-09 08:14:59 +0600

    update undefined.js

---
+++ JavaScript's Core Concept /undefined.js
@@ -0,0 +1,30 @@
+//JavaScript Undefined 
+//Problem 1 
+let text;
+console.log(text);
+
+//Problem 2 
+function sumOf(a, b) {
+  return
+}
+
+console.log(sumOf(5, 6));
+
+
+//Problem 3 
+let arr = [10, 20, 30, 40, 50];
+arr.splice(2, 1);
+console.log(arr);
+console.log(arr[2]);
+
+//Problem 4 
+function thirdPramiter(a, b, c) {
+  return a + b + c;
+}
+
+console.log(thirdPramiter(2, 7, 6));
+
+
+//Problem 5 
+let studnt = { name: 'Sahin', roll: 01, marsks: 99 };
+console.log(studnt.marsks);
