66c14b431cf986b85197f2d364102ac342d5d7a8
SahinShazi <rksahinrone@gmail.com>
2025-10-20 07:38:10 +0600

    update typeConstructor.js

---
+++ Chapter/Building Object & Method/typeConstructor.js
@@ -0,0 +1,27 @@
+//JavaScript Buil In Object & Method 
+//Problem 1 
+let start = "786";
+start = Number(start);
+
+console.log(start);
+
+
+//Problem 2 
+let willAttend = 0;
+willAttend = Boolean(willAttend);
+console.log(willAttend);
+
+
+//Problem 3 
+let info = new Object();
+info.city = "Bhola";
+info.country = "Bangladesh";
+
+console.log(info);
+
+
+//Problem 4 
+let number = 1970;
+number = String(number);
+console.log(number);
+console.log(typeof(number));
