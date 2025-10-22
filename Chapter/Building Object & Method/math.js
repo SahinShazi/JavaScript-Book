a5d0905d49df0e20351f469a9c9bb8397f38c647
SahinShazi <rksahinrone@gmail.com>
2025-10-22 09:40:36 +0600

    update math.js

---
+++ Chapter/Building Object & Method/math.js
@@ -0,0 +1,41 @@
+//JavaScript Math Function 
+//Problem 1 
+const math = Math.min(45,11,89,23,56,-12,-56);
+console.log(math);
+
+//Problem 2 
+const math1 = Math.max(21,35,67);
+console.log(math1);
+
+//Problem 3  
+const math2 = Math.round(7.6);
+const math3 = Math.round(7.2);
+console.log(math3);
+console.log(math2);
+
+//Problem 4 
+const math4 = Math.floor(9.8);
+const math5 = Math.floor(5.3);
+
+console.log(math4);
+console.log(math5);
+
+
+//Problem 5
+const math6 = Math.ceil(3.1);
+const math7 = Math.ceil(6.9);
+
+console.log(math6);
+console.log(math7);
+
+//Problem 6 
+const math8 = Math.abs(-34);
+console.log(math8);
+
+//Problem 7 
+function num(numbe) {
+  return Math.round(numbe);
+}
+
+console.log(num(8.9));
+
