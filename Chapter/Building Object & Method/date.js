c032af35a410dff2f3d1bdd1d73ab152c0a11d6a
SahinShazi <rksahinrone@gmail.com>
2025-10-24 08:06:50 +0600

    update date.js

---
+++ Chapter/Building Object & Method/date.js
@@ -0,0 +1,20 @@
+//JavaScript Date
+//Problem 1 
+const now = new Date();
+console.log(now);
+
+//Problem 2 
+const setDate = new Date(2035, 6, 15, 14, 45, 30);
+console.log(setDate);
+
+//Problem 3 
+const year = new Date().getFullYear();
+console.log(year);
+
+//Problem 4 
+const setYear = new Date().setFullYear(2040);
+console.log(setYear);
+
+//Problem 5 
+const getDay = new Date().getDay("2029-02-16");
+console.log(getDay);
