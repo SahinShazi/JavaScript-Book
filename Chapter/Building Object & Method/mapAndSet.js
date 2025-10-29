093e80f9a3a645e572ab68d524d408e5a13633f8
SahinShazi <rksahinrone@gmail.com>
2025-10-29 07:50:16 +0600

    update

--- Chapter/Building Object & Method/mapAndSet.js
+++ Chapter/Building Object & Method/mapAndSet.js
@@ -1,3 +1,4 @@
+//JavaScript Set
 //Example 01
 const number = [1,2,2,3,4,4,5,6,7,8,8,8,9,9];
 const uniqueArray = [...new Set(number)];
@@ -17,3 +18,13 @@ console.log(mySet.has(100));
 
 mySet.delete(10);
 console.log(mySet);
+
+//JavaScript Map
+//Example 03
+const myMap = new Map();
+myMap.set("name", "Sahin");
+myMap.set("age", 25);
+myMap.set("sector", "Web Developer");
+myMap.set("rool", 01);
+
+console.log(myMap);
